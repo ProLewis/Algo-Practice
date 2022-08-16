3c6d26d132aa4e02f2af974e57fddf7d140a1608
Cody Lewis <codyjlewis118@gmail.com>
2022-08-16 16:33:37 -0700

    Update d10.js
    Completed nextBigger Algo

--- d10.js
+++ d10.js
@@ -1,23 +1,48 @@
 //Next bigger number with the same digits
 
 function nextBigger(n){
-    let stringNum  = n.toString();
-    console.log(stringNum);
-    let size = stringNum.length;
-    //console.log(size);
-    for (let i = size - 1; i > 0; i--){
-        if (stringNum[i] > stringNum[i - 1]) {
-            console.log(stringNum[i]);
-            let temp = 
-            [stringNum[i - 1], stringNum[i]] = [stringNum[i], stringNum[i-1]];
-            console.log(stringNum);
-            
+    
+    let arrayNum = Array.from(String(n), Number);
+    console.log(arrayNum);
+    //let stringNum  = n.toString();
+    let nSize = arrayNum.length;
+
+    //looks for a pivot point where a suitable swap can start
+    for (var i = nSize - 1; i > 0; i--){
+        console.log(i);
+        if (arrayNum[i] > arrayNum[i - 1]) {
             break;
         }
+    }
+    let pivot = i - 1;
+    console.log("pivot is: " + arrayNum[pivot]);
+    console.log("i: " + i);
 
+    //Compares the last 2 and if no pivot point is found, returns -1
+    if (i == 0) {
+        return -1;
+    }
+
+    
+    //Finds the smallest value to the right of the pivot, that is still greater than the pivot
+    let smallest = i;
+    for (let j = i; j < nSize; j++) {
+        if (arrayNum[j] > arrayNum[pivot] && arrayNum[j] < arrayNum[smallest]) {
+            smallest = j;
+        }
         
     }
-    //return -1;
+    console.log("Smallest is: " + arrayNum[smallest]);
+
+    //Swaps the pivot and smallest value to its right
+    [arrayNum[smallest], arrayNum[pivot]] = [arrayNum[pivot], arrayNum[smallest]];
+    console.log(arrayNum);
+
+    let output = [];
+    output.push(arrayNum.slice(0, i).join(''));
+    output.push(arrayNum.slice(i).sort().join(''));
+    console.log("Output is: " + output.join(''));
+    return Number(output.join(''));
 }
 
-console.log(nextBigger(12543))
+console.log(nextBigger(68431));
